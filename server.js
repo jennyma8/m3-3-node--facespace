45c0569ee8f128e278816b9ff0d59e39924bd2a2
jenny ma <jenny_lau_8@hotmail.com>
2020-06-06 15:38:14 -0400

    1.1

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const morgan = require('morgan');
 
 const { users } = require('./data/users');
 
-let currentUser = {};
-
 // declare the 404 function
 const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
+};
+
+const handleHomepage = (req, res) => {
+   res.status(200).send('homepage');
+   res.render('pages/homepage', {
+     users: users
+   })
 };
 
 // -----------------------------------------------------
@@ -21,8 +26,10 @@ express()
   .set('view engine', 'ejs')
 
   // endpoints
+.get('/', handleHomepage)
 
   // a catchall endpoint that will send the 404 message.
   .get('*', handleFourOhFour)
+
 
   .listen(8000, () => console.log('Listening on port 8000'));
